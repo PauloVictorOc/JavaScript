d7497f9ef54fcf58c13a907805de5d359a188ac2
viCtor <alphap2708@gmail.com>
2026-07-31 15:40:10 -0300

    comandos
    if e switch

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 22
+if (idade == 16 || idade > 65){
+    console.log('Voto não obrigatório')
+}
+else if (idade >= 18){
+    console.log('Voto obrigatório')
+}
+else {
+    console.log('Não vota')
+}
